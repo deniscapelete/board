23a9ef909f430674172a9c3d09153718fcc05b6c
Denis Capelete <capelete.denis@gmail.com>
2026-02-17 20:08:16 -0300

    feat: Roteamento e Layouts no Next.js

---
+++ src/app/(board)/layout.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function BoardLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className="max-w-405 w-full mx-auto p-10 flex flex-col gap-8 h-dvh">
+      <div>Header</div>
+
+      {children}
+    </div>
+  );
+}
