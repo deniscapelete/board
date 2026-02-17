bf5a0766910bbda07dfa9cd6fe4034340adeae61
Denis Capelete <capelete.denis@gmail.com>
2026-02-17 20:39:15 -0300

    feat: Estrutura do Cabeçalho

---
+++ src/app/(board)/header.tsx
@@ -0,0 +1,32 @@
+import { Input } from "@/components/input";
+import { LogInIcon, Search } from "lucide-react";
+
+export function Header() {
+  return (
+    <div className="max-w-225 mx-auto w-full flex items-center justify-between">
+      <div className="space-y-1">
+        <h1 className="font-semibold text-xl">Product Roadmap</h1>
+        <p className="text-sm text-navy-400">
+          Follow the development progress of our entire plataform.
+        </p>
+      </div>
+
+      <div className="flex items-center gap-4">
+        <div className="relative">
+          <Search className="size-4 absolute text-navy-200 left-2.5 top-1/2 -translate-y-1/2 pointer-events-none" />
+          <Input
+            type='text'
+            placeholder="Seach for features..."
+            className="w-67.5 pl-8"
+          />
+        </div>
+
+        <button type="button"
+          className="size-8 rounded-full bg-navy-700 border border-navy-500 flex items-center justify-center hover:bg-navy-600 transition-colors duration-150 cursor-pointer"
+        >
+          <LogInIcon className="size-3.5 text-navy-200" />
+        </button>
+      </div>
+    </div>
+  )
+}
--- src/app/(board)/layout.tsx
+++ src/app/(board)/layout.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Header } from "./header";
 
 export default function BoardLayout({
   children,
@@ -7,7 +8,7 @@ export default function BoardLayout({
 }>) {
   return (
     <div className="max-w-405 w-full mx-auto p-10 flex flex-col gap-8 h-dvh">
-      <div>Header</div>
+      <Header />
 
       {children}
     </div>
