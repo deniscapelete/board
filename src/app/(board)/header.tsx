a0df96717547666ee955a95403d5906d91e67d64
Denis Capelete <capelete.denis@gmail.com>
2026-02-22 19:52:18 -0300

    Search Parameters com Nuqs

--- src/app/(board)/header.tsx
+++ src/app/(board)/header.tsx
@@ -1,7 +1,19 @@
+'use client';
+
 import { Input } from "@/components/input";
 import { LogInIcon, Search } from "lucide-react";
+import { useQueryState, parseAsString, debounce } from "nuqs";
+import { ChangeEvent } from "react";
 
 export function Header() {
+  const [search, setSearch] = useQueryState("q", parseAsString.withDefault(""));
+
+  function handleSearchUpdate(event: ChangeEvent<HTMLInputElement>) {
+    setSearch(event.target.value, {
+      limitUrlUpdates: event.target.value !== '' ? debounce(500) : undefined,
+    });
+  }
+
   return (
     <div className="max-w-225 mx-auto w-full flex items-center justify-between">
       <div className="space-y-1">
@@ -18,6 +30,8 @@ export function Header() {
             type='text'
             placeholder="Seach for features..."
             className="w-67.5 pl-8"
+            value={search}
+            onChange={handleSearchUpdate}
           />
         </div>
 
